5c57e1d2194701279e746087a26acb54bd005467
GbemisolaD <gbemisolaolaniyan@gmail.com>
2022-09-09 20:51:11 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Get a number's last digit and compare with 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
