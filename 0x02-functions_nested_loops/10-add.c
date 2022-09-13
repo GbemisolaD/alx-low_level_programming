94678c3ad85da30a5c744ad8ac6541a5522b13f9
GbemisolaD <gbemisolaolaniyan@gmail.com>
2022-09-13 22:51:10 +0000

    Cooodes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints the addition of two numbers
+ * @i: prints int i
+ * @k: prints ink k
+ * Return: Always 0
+ */
+
+int add(int i, int k)
+{
+	return (i + k);
+}
