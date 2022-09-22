e8e6537577dfa21ffcdac1a3d6fdf80768d3afcc
GbemisolaD <gbemisolaolaniyan@gmail.com>
2022-09-22 08:40:57 +0000

    Strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *_strncat: concatenates specified values
+ * @src: source string
+ * @dest: destination string
+ * @n: number of values
+ * Description: concatenates n number of values
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	if (dest[i - 1] != '\0')
+		dest[i] = '\0';
+
+	return (dest);
+}
