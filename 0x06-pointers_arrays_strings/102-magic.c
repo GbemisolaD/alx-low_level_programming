20d039fceae5acbde778de74989eec5fd53cf818
GbemisolaD <gbemisolaolaniyan@gmail.com>
2022-09-23 21:10:05 +0000

    Hope this works

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,6 +9,13 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 
+	/*
+	 * I am trying to ensure the code is on line 19
+	 * I sincerely hope this works
+	 * FIngers crossed
+	 * You must compile
+	 * Grrr
+	 */
 	*(p + 5) = 98;
 
 	printf("a[2] = %d\n", a[2]);
