1a6e4d11accc2efd0547eaf01ae194e17ddfdca2
GbemisolaD <gbemisolaolaniyan@gmail.com>
2022-10-25 09:44:07 +0000

    Advanced Tasks

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node 
+ * add_nodeint - adds a new node
  * @head: pointer to the first node
  * @n: data to insert
  * Return: pointer to new node
