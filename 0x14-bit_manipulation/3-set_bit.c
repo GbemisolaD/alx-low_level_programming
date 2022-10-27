e1fb1c21c7b2eeb9d0f556b07b7ff28f858dbece
GbemisolaD <gbemisolaolaniyan@gmail.com>
2022-10-27 21:11:06 +0000

    Tasks

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit
+ * @n: a pointer to the bit
+ * @index: the index to set value
+ * Return: -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= (1 << index);
+
+	return (1);
+}
